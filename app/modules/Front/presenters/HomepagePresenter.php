3944aa78fd0b76ecc442cd6c523794d628ba83a3
Petr Lukáš <seegrcz@gmail.com>
2022-10-25 12:01:51 +0200

    Add rozvaha

--- app/modules/Front/presenters/HomepagePresenter.php
+++ app/modules/Front/presenters/HomepagePresenter.php
@@ -33,7 +33,7 @@ final class HomepagePresenter extends BasePresenter
 	const VZAZ_ID = 'vzaz';
 	const ROZVAHA_PREFIX = 'ac_cr_rozvaha_';
 	const VZAZ_PREFIX = 'ac_cr_vzaz_';
-	const ROZVAHA_LIST = [2015, 2016, 2017, 2019, 2020, 2021];
+	const ROZVAHA_LIST = [2015, 2016, 2017, 2018, 2019, 2020, 2021];
 	const VZAZ_LIST = [2015, 2016, 2017, 2018, 2019, 2020, 2021];
 
 	const VYPIS = 'vypis';
